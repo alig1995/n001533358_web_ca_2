919b88dfd4b8ddcb82dd2b8464ee3658bf472bf0
Sean Boyle <sean94@gmail.com>
2019-03-18 23:45:50 +0000

    Added first Stage of OAuth authenication

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 //server dependencies
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
+
 //creating an instanse of the server
 const server = express();
 
-
-server.get('/',(req,res)=>{
-  res.send({bye:'alig1995'})
-})
-
+//setup what straegy passport should use
+passport.use(
+  new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+  },
+  (accessToken,refreshToken,profile,done)=>{
+    console.log('Access Token:',accessToken);
+    console.log('Refresh Token:',refreshToken);
+    console.log('Profile:',profile);
+  })
+)
+//pass the user to google
+server.get(
+  '/auth/google',
+  passport.authenticate('google',{
+    scope:['profile','email']
+  })
+)
+server.get('/auth/google/callback', passport.authenticate('google'))
 const port = process.env.PORT || 5000
 server.listen(port,()=>{
   console.log(`Connected on Port ${port}`);
